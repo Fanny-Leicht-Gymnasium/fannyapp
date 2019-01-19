cc79ac55b21fefcee7ceeb9a3adc823ded6aff1f
dorian <dorian@itsblue.de>
2019-01-19 23:03:50 +0100

    add License

--- sources/filtermodel.cpp
+++ sources/filtermodel.cpp
@@ -1,3 +1,21 @@
+/*
+    Fannyapp - Application to view the cover plan of the Fanny-Leicht-Gymnasium ins Stuttgart Vaihingen, Germany
+    Copyright (C) 2019  Itsblue Development <development@itsblue.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "headers/filtermodel.h"
 
 FilterModel::FilterModel(QObject *parent ) : QAbstractListModel(parent)
